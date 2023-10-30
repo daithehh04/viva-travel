ebb23c44fed49a05f0d49f2f181bd9b87b6f4762
SyxNeet <110085910+SyxNeet@users.noreply.github.com>
2023-10-30 09:35:38 +0700

    update listaction our tours

--- src/pageComponent/Destination/SectionActions.js
+++ src/pageComponent/Destination/SectionActions.js
@@ -6,47 +6,15 @@ import imgAction5 from '@/assets/images/Ourtour_Action5.svg'
 import imgAction6 from '@/assets/images/Ourtour_Action6.svg'
 
 import ListAction from './ListAction'
-function SectionActions() {
-  const listActions = [
-    {
-      icon: imgAction1,
-      title: 'POPULATION',
-      desc: '93 Millions'
-    },
-    {
-      icon: imgAction2,
-      title: 'AREA',
-      desc: '330,210 km2'
-    },
-    {
-      icon: imgAction3,
-      title: 'LANGUAGE',
-      desc: 'Vietnamese'
-    },
-    {
-      icon: imgAction4,
-      title: 'CURRENCY',
-      desc: 'Vietnam Dong (VND)'
-    },
-    {
-      icon: imgAction5,
-      title: 'WEATHER',
-      desc: 'Tropical'
-    },
-    {
-      icon: imgAction6,
-      title: 'TIMEZONE',
-      desc: 'GMT +7'
-    }
-  ]
+function SectionActions({ listActions }) {
   return (
     <div className='md:px-[10.25vw] justify-between px-[8vw] md:rounded-[0] rounded-[4.27vw] md:mt-[4vw] relative md:top-0 top-[-5vw] bg-white md:pt-[0] pt-[16.53vw] md:pb-[0] pb-[15vw] grid max-md:grid-cols-3 max-md:gap-y-[6.53vw] max-md:gap-x-[14.93vw] grid-cols-6 gap-[7.38vw] md:grid-rows-1'>
       {listActions?.map((action, index) => (
         <ListAction
           key={index}
-          icon={action.icon}
-          title={action.title}
-          desc={action.desc}
+          icon={action?.img?.sourceUrl}
+          title={action?.name}
+          desc={action?.desc}
         />
       ))}
     </div>
